6571073f48c015af3a26749e61e8439fa526a056
Wills Stern <willsstern@gmail.com>
2020-09-12 12:06:32 -0700

    ReactPlayer ReactJS component test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import {Button} from 'semantic-ui-react';
+import { Divider } from 'semantic-ui-react';
+import ReactPlayer from 'react-player';
 import './App.css';
 
 class App extends Component {
   render = () => {
     return (
       <div className="App">
+
+        <Divider />
         
-        <Button>Test 2</Button>
+        <ReactPlayer
+        
+          url = 'https://youtu.be/MAu9BNxzpcQ'
+          controls = 'true'
+          light = 'true'
+          playing = 'true'
+
+        />
+
+        <Divider />
 
       </div>
     );
